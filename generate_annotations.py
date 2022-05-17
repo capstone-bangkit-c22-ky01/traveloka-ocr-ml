4a7fc89ae4372ffa10c217b17422f0d9285e15ce
Erland366 <68678137+Erland366@users.noreply.github.com>
2022-05-17 21:59:32 +0800

    Black .

--- generate_annotations.py
+++ generate_annotations.py
@@ -1,22 +1,25 @@
 import os
 import pandas as pd
 
-def generate_annotation(df, annotation_base =  '/data', separator = ' '):
+
+def generate_annotation(df, annotation_base="/data", separator=" "):
     annotations = []
     for _, row in df.iterrows():
         indice, label = row
-        if label == '-':
+        if label == "-":
             continue
-        left = os.path.join(annotation_base, "gambar%04d.jpg"%indice)
+        left = os.path.join(annotation_base, "gambar%04d.jpg" % indice)
         right = label
-        annotation = left+separator+right
+        annotation = left + separator + right
         annotations.append(annotation)
     return annotations
 
-def generate_annotation_file(annotations, file_name = 'annotations.txt', rewrite = False):
-    with open(file_name, 'w' if rewrite else 'a') as file:
+
+def generate_annotation_file(annotations, file_name="annotations.txt", rewrite=False):
+    with open(file_name, "w" if rewrite else "a") as file:
         for annotation in annotations:
-            file.write(annotation+'\n')
+            file.write(annotation + "\n")
+
 
 def main():
     """Generate annotation for training from excel/spreadsheets"""
@@ -24,16 +27,17 @@ def main():
     # Configuration
     excel_path = "./labels.xlsx"
     annotation_base = "/train_name_preprocessed"
-    sheet_name = 'Name'
-    nrows = None # Change None for default
-    annotations_separator = '\\t'
+    sheet_name = "Name"
+    nrows = None  # Change None for default
+    annotations_separator = "\\t"
 
-    df = pd.read_excel(excel_path, sheet_name = sheet_name, header = None, nrows = nrows)
+    df = pd.read_excel(excel_path, sheet_name=sheet_name, header=None, nrows=nrows)
 
     annotations = generate_annotation(df, annotation_base, annotations_separator)
-    generate_annotation_file(annotations, 'gt_name.txt', rewrite = True)
+    generate_annotation_file(annotations, "gt_name.txt", rewrite=True)
     # for a in annotations:
     #     print(a)
+
 
 if __name__ == "__main__":
     main()
--- modules/custom.py
+++ modules/custom.py
@@ -7,58 +7,63 @@ def sparse_categorical_crossentropy(
     y_pred: tf.Tensor,
     from_logits: bool = False,
     ignore_index: int = -1,
-    axis: int = -1
+    axis: int = -1,
 ):
-  y_true = tf.convert_to_tensor(y_true)
-  y_pred = tf.convert_to_tensor(y_pred)
-  y_pred, from_logits = to_logits(y_pred, from_logits)
+    y_true = tf.convert_to_tensor(y_true)
+    y_pred = tf.convert_to_tensor(y_pred)
+    y_pred, from_logits = to_logits(y_pred, from_logits)
 
-  y_true = squeeze(y_true)
-  y_pred = squeeze(y_pred, dims=3)
+    y_true = squeeze(y_true)
+    y_pred = squeeze(y_pred, dims=3)
 
-  valid_mask = y_true != ignore_index
-  indices = tf.where(valid_mask)
+    valid_mask = y_true != ignore_index
+    indices = tf.where(valid_mask)
 
-  ce = tf.losses.sparse_categorical_crossentropy(
-      y_true[valid_mask], y_pred[valid_mask], from_logits, axis
-  )
-  ce = tf.scatter_nd(indices, ce, tf.cast(tf.shape(y_true), tf.int64))
+    ce = tf.losses.sparse_categorical_crossentropy(
+        y_true[valid_mask], y_pred[valid_mask], from_logits, axis
+    )
+    ce = tf.scatter_nd(indices, ce, tf.cast(tf.shape(y_true), tf.int64))
 
-  ce = tf.math.divide_no_nan(
-      tf.reduce_sum(ce, axis=-1),
-      tf.cast(tf.math.count_nonzero(valid_mask, axis=-1), ce.dtype)
-  )
+    ce = tf.math.divide_no_nan(
+        tf.reduce_sum(ce, axis=-1),
+        tf.cast(tf.math.count_nonzero(valid_mask, axis=-1), ce.dtype),
+    )
 
-  return ce
+    return ce
 
 
 def squeeze(y, dims: int = 2):
-  if dims not in (2, 3):
-    raise ValueError(f'Illegal value for parameter dims=`{dims}`. Can only squeeze '
-                     'positional signal, resulting in a tensor with rank 2 or 3.')
-  shape = tf.shape(y)
-  new_shape = [shape[0], -1]
-  if dims == 3:  # keep channels.
-    new_shape += [shape[-1]]
-  return tf.reshape(y, new_shape)
+    if dims not in (2, 3):
+        raise ValueError(
+            f"Illegal value for parameter dims=`{dims}`. Can only squeeze "
+            "positional signal, resulting in a tensor with rank 2 or 3."
+        )
+    shape = tf.shape(y)
+    new_shape = [shape[0], -1]
+    if dims == 3:  # keep channels.
+        new_shape += [shape[-1]]
+    return tf.reshape(y, new_shape)
 
 
 def to_logits(output, from_logits: bool = False):
-  if from_logits:
-    return output, True
-
-  if hasattr(output, '_keras_logits'):
     if from_logits:
-      warnings.warn(
-          '"`dig_logits_if_available` received `from_logits=True`, but '
-          'the `output` argument was produced by a sigmoid or softmax '
-          'activation and thus does not represent logits. Was this intended?"',
-          stacklevel=2)
-    return output._keras_logits, True
+        return output, True
 
-  if (not isinstance(output, (tf.__internal__.EagerTensor, tf.Variable)) and
-      output.op.type in ('Softmax', 'Sigmoid')) and not hasattr(output, '_keras_history'):
-    assert len(output.op.inputs) == 1
-    return output.op.inputs[0], True
+    if hasattr(output, "_keras_logits"):
+        if from_logits:
+            warnings.warn(
+                '"`dig_logits_if_available` received `from_logits=True`, but '
+                "the `output` argument was produced by a sigmoid or softmax "
+                'activation and thus does not represent logits. Was this intended?"',
+                stacklevel=2,
+            )
+        return output._keras_logits, True
 
-  return output, False
+    if (
+        not isinstance(output, (tf.__internal__.EagerTensor, tf.Variable))
+        and output.op.type in ("Softmax", "Sigmoid")
+    ) and not hasattr(output, "_keras_history"):
+        assert len(output.op.inputs) == 1
+        return output.op.inputs[0], True
+
+    return output, False
