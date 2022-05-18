5b5ea88b80eee40a912b73640655632b30897218
Erland366 <68678137+Erland366@users.noreply.github.com>
2022-05-18 19:38:56 +0800

    "Completed" train.py and test.py

--- test.py
+++ test.py
@@ -1,125 +1,233 @@
 import argparse
+import os
+import re
+import string
+import time
 
+import tensorflow as tf
+from nltk.metrics.distance import edit_distance
+from tensorflow import keras
+
+from dataset import AlignCollate, hierarchical_dataset, tensorflow_dataloader
 from model import Model
+from modules.custom import custom_sparse_categorical_crossentropy
+from utils import Averager, CTCLabelConverter
 
 
-def main():
+def benchmark_all_eval(model, criterion, converter, opt, calculate_infer_time=False):
+    return None
+
+
+def validation(model, criterion, evaluation_loader, converter, opt):
+    """validation or evaluation"""
+    n_correct = 0
+    norm_ED = 0
+    length_of_data = 0
+    infer_time = 0
+    valid_loss_avg = Averager()
+
+    for i, (image_tensors, labels) in enumerate(evaluation_loader):
+        batch_size = image_tensors.shape[0]
+        length_of_data = length_of_data + batch_size
+        image = image_tensors
+
+        length_for_pred = tf.constant(
+            [opt.batch_max_length] * batch_size, dtype=tf.int32
+        )
+        text_for_pred = tf.zeros(
+            shape=[batch_size, opt.batch_max_length + 1], dtype=tf.float64
+        )
+
+        text_for_loss, length_for_loss = converter.encode(
+            labels, batch_max_length=opt.batch_max_length
+        )
+        start_time = time.time()
+
+        if "CTC" in opt.Prediction:
+            preds = model(image, text_for_pred)
+            forward_time = time.time() - start_time
+
+            preds_size = tf.constant([preds.shape[1]] * batch_size, dtype=tf.int32)
+            if opt.baiduCTC:
+                preds = tf.transpose(preds, perm=[1, 0, 2])
+                cost = (
+                    criterion(
+                        labels=preds,
+                        logits=text_for_loss,
+                        label_length=preds_size,
+                        logit_length=length_for_loss,
+                    )
+                    / batch_size
+                )
+            else:
+                preds = tf.nn.log_softmax(preds, axis=2)
+                preds = tf.transpose(preds, perm=[1, 0, 2])
+                cost = criterion(
+                    labels=preds,
+                    logits=text_for_loss,
+                    label_length=preds_size,
+                    logit_length=length_for_loss,
+                )
+
+            # Select max probabilty (greedy decoding) then decode index to character
+            if opt.baiduCTC:
+                preds_index = tf.math.argmax(preds, axis=2)
+                preds_index = tf.reshape(preds_index, shape=[-1])
+            else:
+                preds_index = tf.math.argmax(preds, axis=2)
+            preds_str = converter.decode(preds_index.data, preds_size.data)
+
+        else:
+            preds = model(image, text_for_pred, training=False)
+            forward_time = time.time() - start_time
+
+            preds = preds[:, : text_for_loss.shape[1] - 1, :]
+            target = text_for_loss[:, 1:]
+            cost = criterion(
+                tf.reshape(preds, shape=[-1, preds.shape[-1]]),
+                tf.reshape(target, shape=[-1]),
+            )
+
+            preds_index = tf.math.argmax(preds, axis=2)
+            preds_str = converter.decode(preds_index, length_for_pred)
+            labels = converter.decode(text_for_loss[:, 1:], length_for_loss)
+
+        infer_time += forward_time
+        valid_loss_avg.add(cost)
+
+        preds_probs = tf.nn.softmax(preds, axis=2)
+        preds_max_prob = tf.math.reduce_max(preds_probs, axis=2)
+
+        confidence_score_list = []
+        for gt, pred, pred_max_prob in zip(labels, preds_str, preds_max_prob):
+            if "Attn" in opt.Prediction:
+                gt = gt[: gt.find("[s]")]
+                pred_EOS = pred.find("[s]")
+                pred = pred[:pred_EOS]  # prune after "end of sentence" token ([s])
+                pred_max_prob = pred_max_prob[:pred_EOS]
+
+            # To evaluate "case sensitive model" with alphanumeric and case insensitve setting.
+            if opt.sensitive and opt.data_filtering_off:
+                pred = pred.lower()
+                gt = gt.lower()
+                alphanumeric_case_insensitve = "0123456789abcdefghijklmnopqrstuvwxyz"
+                out_of_alphanumeric_case_insensitve = (
+                    f"[^{alphanumeric_case_insensitve}]"
+                )
+                pred = re.sub(out_of_alphanumeric_case_insensitve, "", pred)
+                gt = re.sub(out_of_alphanumeric_case_insensitve, "", gt)
+
+            if pred == gt:
+                n_correct += 1
+
+            """
+            (old version) ICDAR2017 DOST Normalized Edit Distance https://rrc.cvc.uab.es/?ch=7&com=tasks
+            "For each word we calculate the normalized edit distance to the length of the ground truth transcription."
+            if len(gt) == 0:
+                norm_ED += 1
+            else:
+                norm_ED += edit_distance(pred, gt) / len(gt)
+            """
+
+            # ICDAR2019 Normalized Edit Distance
+            if len(gt) == 0 or len(pred) == 0:
+                norm_ED += 0
+            elif len(gt) > len(pred):
+                norm_ED += 1 - edit_distance(pred, gt) / len(gt)
+            else:
+                norm_ED += 1 - edit_distance(pred, gt) / len(pred)
+
+            # calculate confidence score (= multiply of pred_max_prob)
+            try:
+                confidence_score = tf.math.cumprod(pred_max_prob, axis=0)[-1]
+            except:
+                confidence_score = 0  # for empty pred case, when prune after "end of sentence" token ([s])
+            confidence_score_list.append(confidence_score)
+            # print(pred, gt, pred==gt, confidence_score)
+
+    accuracy = n_correct / float(length_of_data) * 100
+    norm_ED = norm_ED / float(length_of_data)  # ICDAR2019 Normalized Edit Distance
+
+    return (
+        valid_loss_avg.val(),
+        accuracy,
+        norm_ED,
+        preds_str,
+        confidence_score_list,
+        labels,
+        infer_time,
+        length_of_data,
+    )
+    
+def test(opt):
+    """ model configuration """
+    converter = CTCLabelConverter(opt.character)
+    opt.num_class = len(converter.character)
+    
+    if opt.rgb:
+        opt.input_channel = 3
+        
+    model = Model(opt)
+    
+    print("loading pretrained model from %s" % opt.saved_model)
+    model = keras.models.load_model(opt.saved_model)
+    opt.exp_name = '_'.join(opt.saved_model.split('/')[1:])
+    
+    os.makedirs(f"./result/{opt.exp_name}", exist_ok=True)
+    os.system(f"cp {opt.saved_model} ./result/{opt.exp_name}/")
+    
+    if "CTC" in opt.Prediction:
+        criterion = tf.nn.ctc_loss
+    else:
+        # kalo gak yang sparse ya yang biasa
+        # selalu gunakan ignored_index=0 disini
+        criterion = custom_sparse_categorical_crossentropy
+        
+    log = open(f"./result/{opt.exp_name}/log_evaluation.txt", "a")
+    AlignCollate_evaluation = AlignCollate(imgH=opt.imgH, imgW=opt.imgW, keep_ratio_with_pad=opt.PAD)
+    eval_data, eval_data_log = hierarchical_dataset(root=opt.eval_data, opt=opt)
+    evaluation_loader = tensorflow_dataloader(eval_data, batch_size=opt.batch_size, shuffle=False, num_workers=int(opt.workers), collate_fn=AlignCollate_evaluation)
+    _, accuracy_by_best_model, _, _, _, _, _, _ = validation(
+            model, criterion, evaluation_loader, converter, opt)
+    log.write(eval_data_log)
+    print(f"{accuracy_by_best_model:0.3f}")
+    log.write(f"{accuracy_by_best_model:0.3f}\n")
+    log.close()
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--exp_name", help="Where to store logs and models")
-    # parser.add_argument('--train_data', required=True, help='path to training dataset')
-    # parser.add_argument('--valid_data', required=True, help='path to validation dataset')
-    parser.add_argument(
-        "--manualSeed", type=int, default=1111, help="for random seed setting"
-    )
-    parser.add_argument(
-        "--workers", type=int, help="number of data loading workers", default=4
-    )
-    parser.add_argument("--batch_size", type=int, default=192, help="input batch size")
-    parser.add_argument(
-        "--num_iter", type=int, default=300000, help="number of iterations to train for"
-    )
-    parser.add_argument(
-        "--valInterval", type=int, default=2000, help="Interval between each validation"
-    )
-    parser.add_argument(
-        "--saved_model", default="", help="path to model to continue training"
-    )
-    parser.add_argument("--FT", action="store_true", help="whether to do fine-tuning")
-    parser.add_argument(
-        "--adam", action="store_true", help="Whether to use adam (default is Adadelta)"
-    )
-    parser.add_argument(
-        "--lr", type=float, default=1, help="learning rate, default=1.0 for Adadelta"
-    )
-    parser.add_argument(
-        "--beta1", type=float, default=0.9, help="beta1 for adam. default=0.9"
-    )
-    parser.add_argument(
-        "--rho",
-        type=float,
-        default=0.95,
-        help="decay rate rho for Adadelta. default=0.95",
-    )
-    parser.add_argument(
-        "--eps", type=float, default=1e-8, help="eps for Adadelta. default=1e-8"
-    )
-    parser.add_argument(
-        "--grad_clip", type=float, default=5, help="gradient clipping value. default=5"
-    )
-    parser.add_argument(
-        "--baiduCTC", action="store_true", help="for data_filtering_off mode"
-    )
+    parser.add_argument('--eval_data', required=True, help='path to evaluation dataset')
+    parser.add_argument('--benchmark_all_eval', action='store_true', help='evaluate 10 benchmark evaluation datasets')
+    parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
+    parser.add_argument('--batch_size', type=int, default=192, help='input batch size')
+    parser.add_argument('--saved_model', required=True, help="path to saved_model to evaluation")
     """ Data processing """
-    parser.add_argument(
-        "--select_data",
-        type=str,
-        default="MJ-ST",
-        help="select training data (default is MJ-ST, which means MJ and ST used as training data)",
-    )
-    parser.add_argument(
-        "--batch_ratio",
-        type=str,
-        default="0.5-0.5",
-        help="assign ratio for each selected data in the batch",
-    )
-    parser.add_argument(
-        "--total_data_usage_ratio",
-        type=str,
-        default="1.0",
-        help="total data usage ratio, this ratio is multiplied to total number of data.",
-    )
-    parser.add_argument(
-        "--batch_max_length", type=int, default=25, help="maximum-label-length"
-    )
-    parser.add_argument(
-        "--imgH", type=int, default=32, help="the height of the input image"
-    )
-    parser.add_argument(
-        "--imgW", type=int, default=100, help="the width of the input image"
-    )
-    parser.add_argument("--rgb", action="store_true", help="use rgb input")
-    parser.add_argument(
-        "--character", type=str, default="0123456789", help="character label"
-    )
-    parser.add_argument(
-        "--sensitive", action="store_true", help="for sensitive character mode"
-    )
-    parser.add_argument(
-        "--PAD",
-        action="store_true",
-        help="whether to keep ratio then pad for image resize",
-    )
-    parser.add_argument(
-        "--data_filtering_off", action="store_true", help="for data_filtering_off mode"
-    )
+    parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length')
+    parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
+    parser.add_argument('--imgW', type=int, default=100, help='the width of the input image')
+    parser.add_argument('--rgb', action='store_true', help='use rgb input')
+    parser.add_argument('--character', type=str, default='0123456789abcdefghijklmnopqrstuvwxyz', help='character label')
+    parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode')
+    parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
+    parser.add_argument('--data_filtering_off', action='store_true', help='for data_filtering_off mode')
+    parser.add_argument('--baiduCTC', action='store_true', help='for data_filtering_off mode')
     """ Model Architecture """
-    parser.add_argument(
-        "--num_fiducial",
-        type=int,
-        default=20,
-        help="number of fiducial points of TPS-STN",
-    )
-    parser.add_argument(
-        "--input_channel",
-        type=int,
-        default=1,
-        help="the number of input channel of Feature extractor",
-    )
-    parser.add_argument(
-        "--output_channel",
-        type=int,
-        default=512,
-        help="the number of output channel of Feature extractor",
-    )
-    parser.add_argument(
-        "--hidden_size", type=int, default=256, help="the size of the LSTM hidden state"
-    )
+    parser.add_argument('--Transformation', type=str, required=True, help='Transformation stage. None|TPS')
+    parser.add_argument('--FeatureExtraction', type=str, required=True, help='FeatureExtraction stage. VGG|RCNN|ResNet')
+    parser.add_argument('--SequenceModeling', type=str, required=True, help='SequenceModeling stage. None|BiLSTM')
+    parser.add_argument('--Prediction', type=str, required=True, help='Prediction stage. CTC|Attn')
+    parser.add_argument('--num_fiducial', type=int, default=20, help='number of fiducial points of TPS-STN')
+    parser.add_argument('--input_channel', type=int, default=1, help='the number of input channel of Feature extractor')
+    parser.add_argument('--output_channel', type=int, default=512,
+                        help='the number of output channel of Feature extractor')
+    parser.add_argument('--hidden_size', type=int, default=256, help='the size of the LSTM hidden state')
 
     opt = parser.parse_args()
-    model = Model(opt)
-    model.build(input_shape=[[1, 1, 32, 100]])
-    print(model.summary())
 
+    """ vocab / character number configuration """
+    if opt.sensitive:
+        opt.character = string.printable[:-6]  # same with ASTER setting (use 94 char).
 
-if __name__ == "__main__":
-    main()
+    opt.num_gpu = len(tf.config.list_physical_devices("GPU"))
+
+    test(opt)
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import random
 import string
 import sys
 import time
+from test import validation
 
 import numpy as np
 import tensorflow as tf
@@ -141,4 +142,137 @@ def train(opt):
         optimizer.apply_gradients(zip(gradients, variables))
         
         loss_avg.add(cost)
-        # continue to validation
+        # validation part
+        
+        if (iteration + 1) % opt.valInterval == 0 or iteration == 0: # To see training progress, we also conduct validation when 'iteration == 0' 
+            elapsed_time = time.time() - start_time
+            
+            with open(f"./saved_models/{opt.exp_name}/log_train.txt", "a") as log:
+                valid_loss, current_accuracy, current_norm_ED, preds, confidence_score, labels, infer_time, length_of_data = validation(
+                        model, criterion, valid_loader, converter, opt)
+                loss_log = f'[{iteration+1}/{opt.num_iter}] Train loss: {loss_avg.val():0.5f}, Valid loss: {valid_loss:0.5f}, Elapsed_time: {elapsed_time:0.5f}'
+                loss_avg.reset()
+
+                current_model_log = f'{"Current_accuracy":17s}: {current_accuracy:0.3f}, {"Current_norm_ED":17s}: {current_norm_ED:0.2f}'
+                
+                if current_accuracy > best_accuracy:
+                    best_accuracy = current_accuracy
+                    model.save(f"./saved_models/{opt.exp_name}/best_accuracy")
+                if current_norm_ED > best_norm_ED:
+                    best_norm_ED = current_norm_ED
+                    model.save(f"./saved_models/{opt.exp_name}/best_norm_ED")
+                               
+                best_model_log = f'{"Best_accuracy":17s}: {best_accuracy:0.3f}, {"Best_norm_ED":17s}: {best_norm_ED:0.2f}'
+
+                loss_model_log = f"{loss_log}\n{current_model_log}\n{best_model_log}"
+                print(loss_model_log)
+                log.write(loss_model_log + "\n")
+                
+                dashed_line = '-' * 80
+                head = f'{"Ground Truth":25s} | {"Prediction":25s} | Confidence Score & T/F'
+                predicted_result_log = f'{dashed_line}\n{head}\n{dashed_line}\n'
+                for gt, pred, confidence in zip(labels[:5], preds[:5], confidence_score[:5]):
+                    if 'Attn' in opt.Prediction:
+                        gt = gt[:gt.find('[s]')]
+                        pred = pred[:pred.find('[s]')]
+                    
+                    predicted_result_log += f'{gt:25s} | {pred:25s} | {confidence:0.4f}\t{str(pred == gt)}\n'
+                predicted_result_log += f'{dashed_line}'
+                print(predicted_result_log)
+                log.write(predicted_result_log + '\n')
+
+        # save model per 1e+5 iter.
+        if (iteration + 1) % 1e+5 == 0:
+            model.save(f'./saved_models/{opt.exp_name}/iter_{iteration+1}')
+            
+        if (iteration + 1) == opt.num_iter:
+            print('end the training')
+            sys.exit()
+        iteration += 1
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp_name', help='Where to store logs and models')
+    parser.add_argument('--train_data', required=True, help='path to training dataset')
+    parser.add_argument('--valid_data', required=True, help='path to validation dataset')
+    parser.add_argument('--manualSeed', type=int, default=1111, help='for random seed setting')
+    parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
+    parser.add_argument('--batch_size', type=int, default=192, help='input batch size')
+    parser.add_argument('--num_iter', type=int, default=300000, help='number of iterations to train for')
+    parser.add_argument('--valInterval', type=int, default=2000, help='Interval between each validation')
+    parser.add_argument('--saved_model', default='', help="path to model to continue training")
+    parser.add_argument('--FT', action='store_true', help='whether to do fine-tuning')
+    parser.add_argument('--adam', action='store_true', help='Whether to use adam (default is Adadelta)')
+    parser.add_argument('--lr', type=float, default=1, help='learning rate, default=1.0 for Adadelta')
+    parser.add_argument('--beta1', type=float, default=0.9, help='beta1 for adam. default=0.9')
+    parser.add_argument('--rho', type=float, default=0.95, help='decay rate rho for Adadelta. default=0.95')
+    parser.add_argument('--eps', type=float, default=1e-8, help='eps for Adadelta. default=1e-8')
+    parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping value. default=5')
+    parser.add_argument('--baiduCTC', action='store_true', help='for data_filtering_off mode')
+    """ Data processing """
+    parser.add_argument('--select_data', type=str, default='MJ-ST',
+                        help='select training data (default is MJ-ST, which means MJ and ST used as training data)')
+    parser.add_argument('--batch_ratio', type=str, default='0.5-0.5',
+                        help='assign ratio for each selected data in the batch')
+    parser.add_argument('--total_data_usage_ratio', type=str, default='1.0',
+                        help='total data usage ratio, this ratio is multiplied to total number of data.')
+    parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length')
+    parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
+    parser.add_argument('--imgW', type=int, default=100, help='the width of the input image')
+    parser.add_argument('--rgb', action='store_true', help='use rgb input')
+    parser.add_argument('--character', type=str,
+                        default='0123456789abcdefghijklmnopqrstuvwxyz', help='character label')
+    parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode')
+    parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
+    parser.add_argument('--data_filtering_off', action='store_true', help='for data_filtering_off mode')
+    """ Model Architecture """
+    parser.add_argument('--Transformation', type=str, required=True, help='Transformation stage. None|TPS')
+    parser.add_argument('--FeatureExtraction', type=str, required=True,
+                        help='FeatureExtraction stage. VGG|RCNN|ResNet')
+    parser.add_argument('--SequenceModeling', type=str, required=True, help='SequenceModeling stage. None|BiLSTM')
+    parser.add_argument('--Prediction', type=str, required=True, help='Prediction stage. CTC|Attn')
+    parser.add_argument('--num_fiducial', type=int, default=20, help='number of fiducial points of TPS-STN')
+    parser.add_argument('--input_channel', type=int, default=1,
+                        help='the number of input channel of Feature extractor')
+    parser.add_argument('--output_channel', type=int, default=512,
+                        help='the number of output channel of Feature extractor')
+    parser.add_argument('--hidden_size', type=int, default=256, help='the size of the LSTM hidden state')
+
+    opt = parser.parse_args()
+
+    if not opt.exp_name:
+        opt.exp_name = f'{opt.Transformation}-{opt.FeatureExtraction}-{opt.SequenceModeling}-{opt.Prediction}'
+        opt.exp_name += f'-Seed{opt.manualSeed}'
+        # print(opt.exp_name)
+
+    os.makedirs(f'./saved_models/{opt.exp_name}', exist_ok=True)
+
+    """ vocab / character number configuration """
+    if opt.sensitive:
+        # opt.character += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        opt.character = string.printable[:-6]  # same with ASTER setting (use 94 char).
+
+    """ Seed and GPU setting """
+    # print("Random Seed: ", opt.manualSeed)
+    random.seed(opt.manualSeed)
+    np.random.seed(opt.manualSeed)
+    tf.random.set_seed(opt.manualSeed)
+
+    opt.num_gpu =len(tf.config.list_physical_devices("GPU"))
+    # print('device count', opt.num_gpu)
+    if opt.num_gpu > 1:
+        print('------ Use multi-GPU setting ------')
+        print('if you stuck too long time with multi-GPU setting, try to set --workers 0')
+        # check multi-GPU issue https://github.com/clovaai/deep-text-recognition-benchmark/issues/1
+        opt.workers = opt.workers * opt.num_gpu
+        opt.batch_size = opt.batch_size * opt.num_gpu
+
+        """ previous version
+        print('To equlize batch stats to 1-GPU setting, the batch_size is multiplied with num_gpu and multiplied batch_size is ', opt.batch_size)
+        opt.batch_size = opt.batch_size * opt.num_gpu
+        print('To equalize the number of epochs to 1-GPU setting, num_iter is divided with num_gpu by default.')
+        If you dont care about it, just commnet out these line.)
+        opt.num_iter = int(opt.num_iter / opt.num_gpu)
+        """
+
+    train(opt)
