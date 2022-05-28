a8088346343fb12535fce89d2e2e9307626f0de5
Erland366 <68678137+Erland366@users.noreply.github.com>
2022-05-28 20:19:45 +0800

    Fixing Flask

--- optical-character-recognition/api/main.py
+++ optical-character-recognition/api/main.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # import io
@@ -7,33 +8,33 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # import numpy as np
 # from PIL import Image
 
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 
-arial_model = keras.models.load_model("optical-character-recognition/saved_models/Arial_Model/best_accuracy")
-nik_model = keras.models.load_model("optical-character-recognition/saved_models/NIK_Model/best_accuracy")
+# arial_model = keras.models.load_model("optical-character-recognition/saved_models/Arial_Model/best_accuracy")
+# nik_model = keras.models.load_model("optical-character-recognition/saved_models/NIK_Model/best_accuracy")
 
 app = Flask(__name__)
+print("Diluar")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        content = request.json
+        print(content)    
+        # file = request.files.get('file')
+        # if file is None or file.filename == "":
+        #     return jsonify({"error": "no file"})
 
-        file = request.files.get('file')
-        if file is None or file.filename == "":
-            return jsonify({"error": "no file"})
-
-        try:
-            image_bytes = file.read()
-            print(image_bytes)
+        # try:
+        #     image_bytes = file.read()
+        #     print(image_bytes)
             # pillow_img = Image.open(io.BytesIO(image_bytes)).convert('L')
             # tensor = transform_image(pillow_img)
             # prediction = predict(tensor)
             # data = {"prediction": int(prediction)}
             # return jsonify(data)
-        except Exception as e:
-            return jsonify({"error": str(e)})
 
-    return "OK"
+        return "OK"
 
 
 if __name__ == "__main__":
