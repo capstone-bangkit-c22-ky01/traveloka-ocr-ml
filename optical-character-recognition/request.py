4ab45f34c7ae67d5ba6abd230b68df46a8e1d10f
Erland366 <68678137+Erland366@users.noreply.github.com>
2022-05-31 20:53:11 +0800

    Add request.py

---
+++ optical-character-recognition/request.py
@@ -0,0 +1,11 @@
+import json
+
+import requests
+
+with open("./testing/input.json", "r") as openfile:
+    json_file = json.loads(openfile.read())
+
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+response = requests.post("http://localhost:5000", data=json.dumps(json_file), headers=headers)
+print(response)
+print(response.text)
